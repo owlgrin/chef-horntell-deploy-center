f40497f0ea582843bcaa918d06a741b4d3f9accb
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-10-06 12:21:00 +0530

    Initial Commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'deploy-center'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures deploy-center'
+long_description 'Installs/Configures deploy-center'
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: deploy-center
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
