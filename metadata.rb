0ddfd28f246a52ed55cdbb1c6f96311fd90174ad
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-12-22 13:54:20 +0530

    added author email

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'deploy-center'
 maintainer       'Mohit Mamoria'
-maintainer_email 'YOUR_EMAIL'
+maintainer_email 'mohit.mamoria@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures deploy-center'
 long_description 'Installs/Configures deploy-center'
